ce66148bc864c1c45f857cf941ed5b623a50c16e
Andrew MacDonald <hello@andrewjmacdonald.ca>
2022-01-08 13:29:33 -0400

    Add setup.sh script

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+mkdir -p ./build
+mkdir -p ./build/res
+cp -r ./res/* ./build/*
+pushd build
+cmake ../
+popd
+echo "All done. \`cd\` into \`build\` and run \`../build.sh\` to build. Then run \`./apex\` to run the engine test."
