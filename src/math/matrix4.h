7ab889321b9bf0a282084efdd8187bc3eff4bf00
Andrew MacDonald <andrewmacdonald971@gmail.com>
2022-04-12 01:45:25 -0300

    Move matrix util functions over to matrix4.h

--- src/math/matrix4.h
+++ src/math/matrix4.h
@@ -8,9 +8,22 @@
 #include <array>
 
 namespace hyperion {
+
+class Vector3;
+class Quaternion;
+
 class Matrix4 {
     friend std::ostream &operator<<(std::ostream &os, const Matrix4 &mat);
 public:
+    static Matrix4 Translation(const Vector3 &);
+    static Matrix4 Rotation(const Quaternion &);
+    static Matrix4 Rotation(const Vector3 &axis, float radians);
+    static Matrix4 Scaling(const Vector3 &);
+    static Matrix4 Perspective(float fov, int w, int h, float n, float f);
+    static Matrix4 Orthographic(float l, float r, float b, float t, float n, float f);
+    static Matrix4 LookAt(const Vector3 &dir, const Vector3 &up);
+    static Matrix4 LookAt(const Vector3 &pos, const Vector3 &target, const Vector3 &up);
+
     std::array<float, 16> values;
 
     Matrix4();
@@ -30,12 +43,12 @@ public:
     Matrix4 &operator*=(float scalar);
     bool operator==(const Matrix4 &other) const;
 
-    float operator()(int i, int j) const { return values[i * 4 + j]; }
-    inline float &operator()(int i, int j) { return values[i * 4 + j]; }
-    inline float At(int i, int j) const { return operator()(i, j); }
-    inline float &At(int i, int j) { return operator()(i, j); }
-    inline constexpr float operator[](int index) const { return values[index]; }
-    inline constexpr float &operator[](int index) { return values[index]; }
+    constexpr float operator()(int i, int j) const { return values[i * 4 + j]; }
+    constexpr float &operator()(int i, int j) { return values[i * 4 + j]; }
+    constexpr float At(int i, int j) const { return operator()(i, j); }
+    constexpr float &At(int i, int j) { return operator()(i, j); }
+    constexpr float operator[](int index) const { return values[index]; }
+    constexpr float &operator[](int index) { return values[index]; }
 
     static Matrix4 Zeroes();
     static Matrix4 Ones();
