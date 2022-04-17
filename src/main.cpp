c1184a96b21369b4987f7a22713ceb33f0d2fea0
Andrew MacDonald <andrewmacdonald971@gmail.com>
2022-04-17 17:05:16 -0300

    TMP test case of octree destructor throwing

--- src/main.cpp
+++ src/main.cpp
@@ -200,13 +200,13 @@ int main()
 
     auto [zombie, sponza, cube_obj] = engine.assets.Load<v2::Node>(
         base_path + "/res/models/ogrexml/dragger_Body.mesh.xml",
-        base_path + "/res/models/sponza/sponza.obj", //San_Miguel/san-miguel-low-poly.obj",
+        base_path + "/res/models/fireplace_room/fireplace_room.obj",//sponza/sponza.obj", //San_Miguel/san-miguel-low-poly.obj",
         base_path + "/res/models/cube.obj"
     );
 
     //sponza->Translate({0, 0, 5});
 
-    sponza->Scale(0.02f);
+    sponza->Scale(0.5f);
     //sponza->Scale(0.1f);
     //sponza->Rotate(Quaternion({1, 0, 0}, MathUtil::DegToRad(90.0f)));
     sponza->Update(&engine);
