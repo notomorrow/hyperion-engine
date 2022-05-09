8c28218b08680cea44f11aac38e1a3113b35c834
Andrew MacDonald <andrewmacdonald971@gmail.com>
2022-05-09 16:09:57 -0300

    Revert changes to debug.h and renderer_result.h

--- src/rendering/backend/renderer_result.h
+++ src/rendering/backend/renderer_result.h
@@ -59,25 +59,25 @@ struct Result {
 #define HYPERION_VK_CHECK(vk_result) \
     do { \
         if ((vk_result) != VK_SUCCESS) \
-            return ::hyperion::renderer::Result(::hyperion::renderer::Result::RENDERER_ERR, HYP_DEBUG_FUNC_SHORT ": " #vk_result " != VK_SUCCESS", int(vk_result)); \
+            return ::hyperion::renderer::Result(::hyperion::renderer::Result::RENDERER_ERR, #vk_result " != VK_SUCCESS", int(vk_result)); \
     } while (0)
 
 #define HYPERION_VK_CHECK_MSG(vk_result, msg) \
     do { \
         if ((vk_result) != VK_SUCCESS) \
-            return ::hyperion::renderer::Result(::hyperion::renderer::Result::RENDERER_ERR, HYP_DEBUG_FUNC_SHORT ": " msg, int(vk_result)); \
+            return ::hyperion::renderer::Result(::hyperion::renderer::Result::RENDERER_ERR, msg, int(vk_result)); \
     } while (0)
 
 #define HYPERION_VK_PASS_ERRORS(vk_result, out_result) \
     do { \
         if ((vk_result) != VK_SUCCESS) \
-            (out_result) = ::hyperion::renderer::Result(::hyperion::renderer::Result::RENDERER_ERR, HYP_DEBUG_FUNC_SHORT ": " #vk_result " != VK_SUCCESS", int(vk_result)); \
+            (out_result) = ::hyperion::renderer::Result(::hyperion::renderer::Result::RENDERER_ERR, #vk_result " != VK_SUCCESS", int(vk_result)); \
     } while (0)
 
 #define HYPERION_VK_PASS_ERRORS_MSG(vk_result, msg, out_result) \
     do { \
         if ((vk_result) != VK_SUCCESS) \
-            (out_result) = ::hyperion::renderer::Result(::hyperion::renderer::Result::RENDERER_ERR, HYP_DEBUG_FUNC_SHORT ": " msg, int(vk_result)); \
+            (out_result) = ::hyperion::renderer::Result(::hyperion::renderer::Result::RENDERER_ERR, msg, int(vk_result)); \
     } while (0)
 
 } // namespace renderer
