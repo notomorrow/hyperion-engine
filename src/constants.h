0d558706136a26a1e62c5eff1006b91fcabca0f8
Andrew MacDonald <hello@andrewjmacdonald.ca>
2022-06-02 00:54:05 -0300

    WIP

---
+++ src/constants.h
@@ -0,0 +1,12 @@
+#ifndef HYPERION_CONSTANTS_H
+#define HYPERION_CONSTANTS_H
+
+#include <types.h>
+
+namespace hyperion {
+
+constexpr uint max_frames_in_flight = 2;
+
+} // namespace hyperion
+
+#endif
