dd648bb1330efda8e822aed6f5272cb0fdfe7137
Andrew MacDonald <hello@andrewjmacdonald.ca>
2022-06-03 23:55:33 -0300

    WIP

--- generate_projects.sh
+++ generate_projects.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 mkdir -p project
+
 if [[ "$OSTYPE" == "darwin"* ]]; then
-cmake -G Xcode
-elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-cmake -G Makefile
-elif [[ "$OSTYPE" == "win32"* ]]; then
-cmake -G "Visual Studio 22"
+# change to correct path if need be
+cmake -GXcode -B./project -D CMAKE_CXX_COMPILER="/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/c++" -D CMAKE_C_COMPILER="/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/cc"
+else
+echo "No project to generate (or not added to this script yet), instead use ./build.sh"
 fi
