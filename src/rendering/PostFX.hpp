9cba2cb19ee551bd0702d77f21fc5ca5441a7c25
Andrew MacDonald <hello@andrewjmacdonald.ca>
2022-07-07 19:45:12 -0300

    More file name changes

---
+++ src/rendering/PostFX.hpp
@@ -0,0 +1,180 @@
+#ifndef HYPERION_V2_POST_FX_H
+#define HYPERION_V2_POST_FX_H
+
+#include "RenderPass.hpp"
+#include "Framebuffer.hpp"
+#include "Shader.hpp"
+#include "Graphics.hpp"
+#include "Mesh.hpp"
+#include "FullScreenPass.hpp"
+
+#include <core/lib/TypeMap.hpp>
+#include <Types.hpp>
+
+#include <rendering/backend/RendererFrame.hpp>
+#include <rendering/backend/RendererStructs.hpp>
+#include <rendering/backend/RendererBuffer.hpp>
+#include <rendering/backend/RendererCommandBuffer.hpp>
+
+
+#include <memory>
+#include <utility>
+
+namespace hyperion::v2 {
+
+using renderer::Frame;
+using renderer::CommandBuffer;
+using renderer::PerFrameData;
+using renderer::VertexAttributeSet;
+using renderer::UniformBuffer;
+
+class Engine;
+
+struct alignas(16) PostProcessingUniforms {
+    UInt32 effect_counts[2]; // pre, post
+    UInt32 last_enabled_indices[2]; // pre, post
+    UInt32 masks[2]; // pre, post
+};
+
+class PostProcessingEffect : public EngineComponentBase<STUB_CLASS(PostProcessingEffect)> {
+public:
+    enum class Stage {
+        PRE_SHADING,
+        POST_SHADING
+    };
+
+    PostProcessingEffect(
+        Stage stage,
+        UInt index,
+        Image::InternalFormat image_format = Image::InternalFormat::TEXTURE_INTERNAL_FORMAT_RGBA8_SRGB
+    );
+    PostProcessingEffect(const PostProcessingEffect &other) = delete;
+    PostProcessingEffect &operator=(const PostProcessingEffect &other) = delete;
+    virtual ~PostProcessingEffect();
+
+    FullScreenPass &GetFullScreenPass()             { return m_full_screen_pass; }
+    const FullScreenPass &GetFullScreenPass() const { return m_full_screen_pass; }
+
+    Ref<Shader> &GetShader()                        { return m_shader; }
+    const Ref<Shader> &GetShader() const            { return m_shader; }
+
+    Stage GetStage() const                          { return m_stage; }
+    UInt GetIndex() const                           { return m_full_screen_pass.GetSubDescriptorIndex(); }
+
+    bool IsEnabled() const                          { return m_is_enabled; }
+    void SetIsEnabled(bool is_enabled)              { m_is_enabled = is_enabled; }
+
+    void Init(Engine *engine);
+
+protected:
+    virtual Ref<Shader> CreateShader(Engine *engine) = 0;
+
+    FullScreenPass m_full_screen_pass;
+
+private:
+    Ref<Shader>    m_shader;
+    Stage          m_stage;
+    bool           m_is_enabled;
+};
+
+class PostProcessing {
+public:
+    static constexpr UInt max_effects_per_stage = sizeof(UInt32) * CHAR_BIT;
+
+    enum DefaultEffectIndices {
+        DEFAULT_EFFECT_INDEX_SSAO,
+        DEFAULT_EFFECT_INDEX_FXAA
+    };
+
+    PostProcessing();
+    PostProcessing(const PostProcessing &) = delete;
+    PostProcessing &operator=(const PostProcessing &) = delete;
+    ~PostProcessing();
+
+    /*! \brief Add an effect to the stack to be processed BEFORE deferred rendering happens.
+    * Note, cannot add new filters after pipeline construction, currently
+     * @param effect A unique_ptr to the class, derived from PostProcessingEffect.
+     */
+    template <class EffectClass>
+    void AddEffect(std::unique_ptr<EffectClass> &&effect)
+    {
+        const PostProcessingEffect::Stage stage = EffectClass::stage;
+
+        if constexpr (stage == PostProcessingEffect::Stage::PRE_SHADING) {
+            AddEffectInternal(std::move(effect), m_pre_effects);
+        } else {
+            AddEffectInternal(std::move(effect), m_post_effects);
+        }
+    }
+
+    /*! \brief Add an effect to the stack to be processed BEFORE deferred rendering happens, constructed by the given arguments.
+     * Note, cannot add new filters after pipeline construction, currently
+     */
+    template <class EffectClass, class ...Args>
+    void AddEffect(Args &&... args)
+    {
+        const PostProcessingEffect::Stage stage = EffectClass::stage;
+
+        if constexpr (stage == PostProcessingEffect::Stage::PRE_SHADING) {
+            AddEffectInternal(std::make_unique<EffectClass>(std::forward<Args>(args)...), m_pre_effects);
+        } else {
+            AddEffectInternal(std::make_unique<EffectClass>(std::forward<Args>(args)...), m_post_effects);
+        }
+    }
+
+    /*! \brief Get an effect added to the list of effects to be applied BEFORE deferred rendering happens */
+    template <class EffectClass>
+    EffectClass *GetEffect() const
+    {
+        const PostProcessingEffect::Stage stage = EffectClass::stage;
+
+        if constexpr (stage == PostProcessingEffect::Stage::PRE_SHADING) {
+            return GetEffectInternal<EffectClass>(m_pre_effects);
+        } else {
+            return GetEffectInternal<EffectClass>(m_post_effects);
+        }
+    }
+
+    void Create(Engine *engine);
+    void Destroy(Engine *engine);
+    void RenderPre(Engine *engine, Frame *frame) const;
+    void RenderPost(Engine *engine, Frame *frame) const;
+
+private:
+    void CreateUniformBuffer(Engine *engine);
+
+    template <class EffectClass>
+    void AddEffectInternal(std::unique_ptr<EffectClass> &&effect, TypeMap<std::unique_ptr<PostProcessingEffect>> &effects)
+    {
+        static_assert(std::is_base_of_v<PostProcessingEffect, EffectClass>, "Type must be a derived class of PostProcessingEffect.");
+
+        if (!effects.Contains<EffectClass>()) {
+            AssertThrowMsg(effects.Size() < max_effects_per_stage, "Maximum number of effects in stage");
+        }
+
+        effects.Set<EffectClass>(std::move(effect));
+    }
+
+    template <class EffectClass>
+    EffectClass *GetEffectInternal(TypeMap<std::unique_ptr<PostProcessingEffect>> &effects) const
+    {
+        static_assert(std::is_base_of_v<PostProcessingEffect, EffectClass>, "Type must be a derived class of PostProcessingEffect.");
+
+        auto it = effects.Find<EffectClass>();
+
+        if (it == effects.End()) {
+            return nullptr;
+        }
+
+        return static_cast<EffectClass *>(it->second.get());
+    }
+
+    TypeMap<std::unique_ptr<PostProcessingEffect>> m_pre_effects;
+    TypeMap<std::unique_ptr<PostProcessingEffect>> m_post_effects;
+    UniformBuffer                                  m_uniform_buffer;
+};
+
+} // namespace hyperion::v2
+
+#endif // HYPERION_V2_POST_FX_H
+
