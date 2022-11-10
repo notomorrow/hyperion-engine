7f1cb20e09b70e840c5485ff647cd549b5829d66
Andrew MacDonald <hello@andrewjmacdonald.ca>
2022-11-10 19:05:20 -0400

    Refactoring language

--- src/script/compiler/type-system/BuiltinTypes.hpp
+++ src/script/compiler/type-system/BuiltinTypes.hpp
@@ -26,11 +26,8 @@ struct BuiltinTypes {
     static const SymbolTypePtr_t STRING;
     static const SymbolTypePtr_t FUNCTION;
     static const SymbolTypePtr_t ARRAY;
-    static const SymbolTypePtr_t TUPLE;
     static const SymbolTypePtr_t VAR_ARGS;
     static const SymbolTypePtr_t NULL_TYPE;
-    static const SymbolTypePtr_t EVENT;
-    static const SymbolTypePtr_t EVENT_IMPL;
     static const SymbolTypePtr_t MODULE_INFO;
     static const SymbolTypePtr_t GENERATOR;
     static const SymbolTypePtr_t BOXED_TYPE;
