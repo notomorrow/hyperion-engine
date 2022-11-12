ea7e805af96a1c136dcc5dae4b44eee1dc157ffd
Andrew MacDonald <hello@andrewjmacdonald.ca>
2022-11-11 21:34:37 -0400

    Fix crash on macos

--- src/Game.cpp
+++ src/Game.cpp
@@ -53,7 +53,7 @@ void Game::Init(Engine *engine)
 void Game::Update(Engine *engine, GameCounter::TickUnit delta)
 {
     engine->GetComponents().Update(engine, delta);
-    engine->GetWorld().Update(engine, delta);
+    engine->GetWorld()->Update(engine, delta);
 
     Logic(engine, delta);
 }
