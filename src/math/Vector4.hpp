973e4b33e652dfb91458140b23ac84dbc8a344dc
emd22 <e@ethanm.ca>
2022-11-20 23:26:53 -0400

    fixed alignment issue

--- src/math/Vector4.hpp
+++ src/math/Vector4.hpp
@@ -27,7 +27,7 @@ public:
     union alignas(16) {
         struct alignas(16) { float x, y, z, w; };
         float values[4];
-        __m128 vector;
+        intrinsics::Float128U vector;
     };
 
     Vector4();
