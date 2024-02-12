3f3641840bd73f9e85d1066e5d4358086263931d
Andrew MacDonald <andrewmacdonald971@gmail.com>
2024-02-12 02:33:41 -0400

    WIP - DescriptorSet2 class

---
+++ src/rendering/backend/RendererDescriptorSet2.hpp
@@ -0,0 +1,133 @@
+#ifndef HYPERION_V2_BACKEND_RENDERER_DESCRIPTOR_SET2_HPP
+#define HYPERION_V2_BACKEND_RENDERER_DESCRIPTOR_SET2_HPP
+
+#include <core/Name.hpp>
+#include <core/lib/Optional.hpp>
+#include <core/lib/RefCountedPtr.hpp>
+#include <core/lib/ArrayMap.hpp>
+#include <core/lib/Mutex.hpp>
+#include <rendering/backend/Platform.hpp>
+#include <rendering/backend/RenderObject.hpp>
+#include <Types.hpp>
+#include <util/Defines.hpp>
+
+
+namespace hyperion {
+namespace renderer {
+
+namespace platform {
+
+template <PlatformType PLATFORM>
+class Device;
+    
+template <PlatformType PLATFORM>
+class Instance;
+
+enum class DescriptorSetElementType
+{
+    UNSET,
+    UNIFORM_BUFFER,
+    UNIFORM_BUFFER_DYNAMIC,
+    STORAGE_BUFFER,
+    STORAGE_BUFFER_DYNAMIC,
+    IMAGE,
+    IMAGE_STORAGE,
+    SAMPLER,
+    TLAS,
+    MAX
+};
+
+template <PlatformType PLATFORM>
+class DescriptorSet2;
+
+template <PlatformType PLATFORM>
+class DescriptorSetLayout
+{
+public:
+    DescriptorSetLayout()                                                   = default;
+    DescriptorSetLayout(const DescriptorSetLayout &other)                   = default;
+    DescriptorSetLayout &operator=(const DescriptorSetLayout &other)        = default;
+    DescriptorSetLayout(DescriptorSetLayout &&other) noexcept               = default;
+    DescriptorSetLayout &operator=(DescriptorSetLayout &&other) noexcept    = default;
+    ~DescriptorSetLayout()                                                  = default;
+
+    DescriptorSet2Ref<PLATFORM> CreateDescriptorSet() const;
+
+    const ArrayMap<String, DescriptorSetElementType> &GetElements() const
+        { return m_elements; }
+
+    void AddElement(const String &name, DescriptorSetElementType type)
+        { m_elements.Insert(name, type); }
+
+    Optional<DescriptorSetElementType> GetElement(const String &name) const
+    {
+        const auto it = m_elements.Find(name);
+
+        if (it == m_elements.End()) {
+            return { };
+        }
+
+        return it->second;
+    }
+
+private:
+    ArrayMap<String, DescriptorSetElementType>  m_elements;
+};
+
+template <PlatformType PLATFORM>
+struct DescriptorSetElement
+{
+    using ValueType = Variant<GPUBufferRef<PLATFORM>, ImageViewRef<PLATFORM>, SamplerRef<PLATFORM>, TLASRef<PLATFORM>>;
+
+    ValueType   value;
+    bool        dirty = false;
+};
+
+template <PlatformType PLATFORM>
+class DescriptorSet2
+{
+public:
+    DescriptorSet2(const DescriptorSetLayout<PLATFORM> &layout);
+    DescriptorSet2(const DescriptorSet2 &other)                 = delete;
+    DescriptorSet2 &operator=(const DescriptorSet2 &other)      = delete;
+    DescriptorSet2(DescriptorSet2 &&other) noexcept             = delete;
+    DescriptorSet2 &operator=(DescriptorSet2 &&other) noexcept  = delete;
+    ~DescriptorSet2();
+    
+    void SetElement(const String &name, const GPUBufferRef<PLATFORM> &ref);
+    void SetElement(const String &name, uint index, const GPUBufferRef<PLATFORM> &ref);
+    
+    void SetElement(const String &name, const ImageViewRef<PLATFORM> &ref);
+    void SetElement(const String &name, uint index, const ImageViewRef<PLATFORM> &ref);
+    
+    void SetElement(const String &name, const SamplerRef<PLATFORM> &ref);
+    void SetElement(const String &name, uint index, const SamplerRef<PLATFORM> &ref);
+    
+    void SetElement(const String &name, const TLASRef<PLATFORM> &ref);
+    void SetElement(const String &name, uint index, const TLASRef<PLATFORM> &ref);
+
+private:
+    DescriptorSetLayout<PLATFORM>                       m_layout;
+    ArrayMap<String, DescriptorSetElement<PLATFORM>>    m_elements;
+};
+
+} // namespace platform
+} // namespace renderer
+} // namespace hyperion
+
+
+#if HYP_VULKAN
+#include <rendering/backend/vulkan/RendererDescriptorSet.hpp>
+#else
+#error Unsupported rendering backend
+#endif
+
+namespace hyperion {
+namespace renderer {
+
+using DescriptorSet2 = platform::DescriptorSet2<Platform::CURRENT>;
+
+} // namespace renderer
+} // namespace hyperion
+
+#endif
---
+++ src/rendering/backend/vulkan/RendererDescriptorSet2.cpp
@@ -0,0 +1,113 @@
+#include <rendering/backend/RendererDescriptorSet2.hpp>
+
+#include <math/MathUtil.hpp>
+
+#include <vulkan/vulkan.h>
+
+namespace hyperion {
+namespace renderer {
+namespace platform {
+
+template <>
+DescriptorSet2Ref<Platform::VULKAN> DescriptorSetLayout<Platform::VULKAN>::CreateDescriptorSet() const
+{
+    auto ref = MakeRenderObject<DescriptorSet2<Platform::VULKAN>>(*this);
+
+    return ref;
+}
+
+template <>
+DescriptorSet2Ref<Platform::VULKAN>::DescriptorSet2Ref(const DescriptorSetLayout<Platform::VULKAN> &layout)
+    : m_layout(layout)
+{
+    // Initial layout of elements
+    for (auto &it : m_layout.GetElements()) {
+        const String &name = it.first;
+        const DescriptorSetElementType type = it.second;
+
+        switch (type) {
+        case DescriptorSetElementType::UNIFORM_BUFFER:          // fallthrough
+        case DescriptorSetElementType::UNIFORM_BUFFER_DYNAMIC:  // fallthrough
+        case DescriptorSetElementType::STORAGE_BUFFER:          // fallthrough
+        case DescriptorSetElementType::STORAGE_BUFFER_DYNAMIC:  // fallthrough
+            SetElement(name, GPUBufferRef<Platform::VULKAN> { });
+
+            break;
+        case DescriptorSetElementType::IMAGE:                   // fallthrough
+        case DescriptorSetElementType::IMAGE_STORAGE:           // fallthrough
+            SetElement(name, ImageViewRef<Platform::VULKAN> { });
+
+            break;
+        case DescriptorSetElementType::SAMPLER:
+            SetElement(name, SamplerRef<Platform::VULKAN> { });
+
+            break;
+        case DescriptorSetElementType::TLAS:
+            SetElement(name, TLASRef<Platform::VULKAN> { });
+
+            break;
+        default:
+            AssertThrowMsg(false, "Unhandled descriptor set element type in layout: %d", int(type));
+
+            break;
+        }
+    }
+}
+
+template <>
+DescriptorSet2Ref<Platform::VULKAN>::~DescriptorSet2Ref()
+{
+}
+
+template <>
+void DescriptorSet2Ref<Platform::VULKAN>::SetElement(const String &name, const GPUBufferRef<Platform::VULKAN> &ref)
+{
+    SetElement(name, 0, ref);
+}
+
+template <>
+void DescriptorSet2Ref<Platform::VULKAN>::SetElement(const String &name, uint index, const GPUBufferRef<Platform::VULKAN> &ref)
+{
+    auto element = m_layout.GetElement(name);
+    AssertThrowMsg(element.HasValue(), "Invalid element: No item with name %s found", name.Data());
+}
+
+template <>
+void DescriptorSet2Ref<Platform::VULKAN>::SetElement(const String &name, const ImageViewRef<Platform::VULKAN> &ref)
+{
+    
+}
+
+template <>
+void DescriptorSet2Ref<Platform::VULKAN>::SetElement(const String &name, uint index, const ImageViewRef<Platform::VULKAN> &ref)
+{
+    
+}
+
+template <>
+void DescriptorSet2Ref<Platform::VULKAN>::SetElement(const String &name, const SamplerRef<Platform::VULKAN> &ref)
+{
+    
+}
+
+template <>
+void DescriptorSet2Ref<Platform::VULKAN>::SetElement(const String &name, uint index, const SamplerRef<Platform::VULKAN> &ref)
+{
+    
+}
+
+template <>
+void DescriptorSet2Ref<Platform::VULKAN>::SetElement(const String &name, const TLASRef<Platform::VULKAN> &ref)
+{
+    
+}
+
+template <>
+void DescriptorSet2Ref<Platform::VULKAN>::SetElement(const String &name, uint index, const TLASRef<Platform::VULKAN> &ref)
+{
+    
+}
+
+} // namespace platform
+} // namespace renderer
+} // namespace hyperion
