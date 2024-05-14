51b9153e019b5b533f0b8811a2f91c7c733b21ab
Andrew MacDonald <mail@ajmd.dev>
2024-05-14 12:09:39 -0300

    Still busted

--- src/rendering/EntityDrawCollection.cpp
+++ src/rendering/EntityDrawCollection.cpp
@@ -297,14 +297,14 @@ void RenderProxyGroup::SetRenderGroup(const Handle<RenderGroup> &render_group)
 
 #pragma region EntityDrawCollection
 
-FixedArray<FlatMap<RenderableAttributeSet, RenderProxyGroup>, PASS_TYPE_MAX> &EntityDrawCollection::GetProxyGroups()
+FixedArray<ArrayMap<RenderableAttributeSet, RenderProxyGroup>, PASS_TYPE_MAX> &EntityDrawCollection::GetProxyGroups()
 {
     Threads::AssertOnThread(ThreadName::THREAD_RENDER);
 
     return m_proxy_groups;
 }
 
-const FixedArray<FlatMap<RenderableAttributeSet, RenderProxyGroup>, PASS_TYPE_MAX> &EntityDrawCollection::GetProxyGroups() const
+const FixedArray<ArrayMap<RenderableAttributeSet, RenderProxyGroup>, PASS_TYPE_MAX> &EntityDrawCollection::GetProxyGroups() const
 {
     Threads::AssertOnThread(ThreadName::THREAD_RENDER);
 
@@ -448,7 +448,7 @@ void RenderList::CollectDrawCalls(
 {
     Threads::AssertOnThread(ThreadName::THREAD_RENDER);
 
-    using IteratorType = FlatMap<RenderableAttributeSet, RenderProxyGroup>::Iterator;
+    using IteratorType = ArrayMap<RenderableAttributeSet, RenderProxyGroup>::Iterator;
 
     Array<IteratorType> iterators;
 
