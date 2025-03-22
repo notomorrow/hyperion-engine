e857a6b88c97cae759f21c3cfdb2fd5ed1a9810f
Andrew MacDonald <mail@ajmd.dev>
2025-03-22 10:36:49 -0300

    Name optimization for C# codegen

--- buildtool/src/generator/generators/CSharpModuleGenerator.cpp
+++ buildtool/src/generator/generators/CSharpModuleGenerator.cpp
@@ -7,6 +7,8 @@
 
 #include <parser/Parser.hpp>
 
+#include <core/Name.hpp>
+
 #include <core/io/ByteWriter.hpp>
 
 namespace hyperion {
@@ -90,21 +92,21 @@ Result CSharpModuleGenerator::Generate_Internal(const Analyzer &analyzer, const 
 
             if (function_type->return_type->IsVoid()) {
                 if (hyp_class.type == HypClassDefinitionType::STRUCT) {
-                    writer.WriteString(HYP_FORMAT("            HypObject.GetMethod(HypClass.GetClass<{}>(), new Name(\"{}\", weak: true)).Invoke(obj{});\n",
-                        hyp_class.name, member.name, method_arg_names.Any() ? ", " + String::Join(method_arg_names, ", ") : ""));
+                    writer.WriteString(HYP_FORMAT("            HypObject.GetMethod(HypClass.GetClass<{}>(), new Name({})).Invoke(obj{});\n",
+                        hyp_class.name, uint64(CreateWeakNameFromDynamicString(member.name)), method_arg_names.Any() ? ", " + String::Join(method_arg_names, ", ") : ""));
                 } else if (hyp_class.type == HypClassDefinitionType::CLASS) {
-                    writer.WriteString(HYP_FORMAT("            obj.GetMethod(new Name(\"{}\", weak: true)).Invoke(obj{});\n",
-                        member.name, method_arg_names.Any() ? ", " + String::Join(method_arg_names, ", ") : ""));
+                    writer.WriteString(HYP_FORMAT("            obj.GetMethod(new Name({})).Invoke(obj{});\n",
+                        uint64(CreateWeakNameFromDynamicString(member.name)), method_arg_names.Any() ? ", " + String::Join(method_arg_names, ", ") : ""));
                 } else {
                     return HYP_MAKE_ERROR(Error, "Unsupported HypClass type");
                 }
             } else {
                 if (hyp_class.type == HypClassDefinitionType::STRUCT) {
-                    writer.WriteString(HYP_FORMAT("            return ({})HypObject.GetMethod(HypClass.GetClass<{}>(), new Name(\"{}\", weak: true)).Invoke(obj{}).GetValue();\n",
-                        return_type_name, hyp_class.name, member.name, method_arg_names.Any() ? ", " + String::Join(method_arg_names, ", ") : ""));
+                    writer.WriteString(HYP_FORMAT("            return ({})HypObject.GetMethod(HypClass.GetClass<{}>(), new Name({})).Invoke(obj{}).GetValue();\n",
+                        return_type_name, hyp_class.name, uint64(CreateWeakNameFromDynamicString(member.name)), method_arg_names.Any() ? ", " + String::Join(method_arg_names, ", ") : ""));
                 } else if (hyp_class.type == HypClassDefinitionType::CLASS) {
-                    writer.WriteString(HYP_FORMAT("            return ({})obj.GetMethod(new Name(\"{}\", weak: true)).Invoke(obj{}).GetValue();\n",
-                        return_type_name, member.name, method_arg_names.Any() ? ", " + String::Join(method_arg_names, ", ") : ""));
+                    writer.WriteString(HYP_FORMAT("            return ({})obj.GetMethod(new Name({})).Invoke(obj{}).GetValue();\n",
+                        return_type_name, uint64(CreateWeakNameFromDynamicString(member.name)), method_arg_names.Any() ? ", " + String::Join(method_arg_names, ", ") : ""));
                 } else {
                     return HYP_MAKE_ERROR(Error, "Unsupported HypClass type");
                 }
