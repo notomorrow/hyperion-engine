d0676b3611d2e434ac34591f2040d7324e28b608
Andrew MacDonald <mail@ajmd.dev>
2025-08-20 21:12:34 -0300

    .hypmodule file to indicate what should build as script modules

--- res/scripts/src/Hyperion/Editor/UI/AddComponent.cs
+++ res/scripts/src/Hyperion/Editor/UI/AddComponent.cs
@@ -1,33 +1,29 @@
 using System;
 
-namespace Hyperion
+namespace Hyperion.Editor.UI;
+
+public class AddComponent : UIEventHandler
 {
-    namespace Editor
+    public override void Init(Entity entity)
     {
-        public class AddComponent : UIEventHandler
-        {
-            public override void Init(Entity entity)
-            {
-                base.Init(entity);
-            }
+        base.Init(entity);
+    }
 
-            [UIEvent(AllowNested = true)]
-            public void LoadComponents()
-            {
-                Logger.Log(LogType.Info, "Load components here");
-            }
+    [UIEvent(AllowNested = true)]
+    public void LoadComponents()
+    {
+        Logger.Log(LogType.Info, "Load components here");
+    }
 
-            [UIEvent(AllowNested = true)]
-            public void AddComponentClicked()
-            {
-                Logger.Log(LogType.Info, "AddComponentClicked");
-            }
+    [UIEvent(AllowNested = true)]
+    public void AddComponentClicked()
+    {
+        Logger.Log(LogType.Info, "AddComponentClicked");
+    }
 
-            [UIEvent(AllowNested = true)]
-            public void CancelClicked()
-            {
-                Logger.Log(LogType.Info, "CancelClicked");
-            }
-        }
+    [UIEvent(AllowNested = true)]
+    public void CancelClicked()
+    {
+        Logger.Log(LogType.Info, "CancelClicked");
     }
 }
--- res/scripts/src/Hyperion/Editor/UI/FileBrowserDialog.cs
+++ res/scripts/src/Hyperion/Editor/UI/FileBrowserDialog.cs
@@ -4,265 +4,261 @@ using System.IO;
 using System.Collections.Generic;
 using Hyperion;
 
-namespace Hyperion
+namespace Hyperion.Editor.UI;
+
+[HypClassBinding(IsDynamic = true)]
+public unsafe struct FileInfo
 {
-    namespace Editor
+    private fixed byte name[2048];
+    private fixed byte path[4096];
+    private bool isDirectory;
+
+    public string Name
     {
-        [HypClassBinding(IsDynamic = true)]
-        public unsafe struct FileInfo
+        get
         {
-            private fixed byte name[2048];
-            private fixed byte path[4096];
-            private bool isDirectory;
-
-            public string Name
+            fixed (byte* namePtr = name)
             {
-                get
-                {
-                    fixed (byte* namePtr = name)
-                    {
-                        return new string((sbyte*)namePtr);
-                    }
-                }
-                set
-                {
-                    fixed (byte* namePtr = name)
-                    {
-                        for (int i = 0; i < 2048; i++)
-                        {
-                            name[i] = 0;
-                        }
-
-                        for (int i = 0; i < value.Length; i++)
-                        {
-                            if (i == 2047)
-                            {
-                                break;
-                            }
-
-                            name[i] = (byte)value[i];
-                        }
-                    }
-                }
+                return new string((sbyte*)namePtr);
             }
-
-            public string Path
+        }
+        set
+        {
+            fixed (byte* namePtr = name)
             {
-                get
+                for (int i = 0; i < 2048; i++)
                 {
-                    fixed (byte* pathPtr = path)
+                    name[i] = 0;
+                }
+
+                for (int i = 0; i < value.Length; i++)
+                {
+                    if (i == 2047)
                     {
-                        return new string((sbyte*)pathPtr);
+                        break;
                     }
-                }
-                set
-                {
-                    fixed (byte* pathPtr = path)
-                    {
-                        for (int i = 0; i < 4096; i++)
-                        {
-                            path[i] = 0;
-                        }
 
-                        for (int i = 0; i < value.Length; i++)
-                        {
-                            if (i == 4095)
-                            {
-                                break;
-                            }
-
-                            path[i] = (byte)value[i];
-                        }
-                    }
-                }
-            }
-
-            public bool IsDirectory
-            {
-                get
-                {
-                    return isDirectory;
-                }
-                set
-                {
-                    isDirectory = value;
+                    name[i] = (byte)value[i];
                 }
             }
         }
+    }
 
-        public class FileInfoUIElementFactory : UIElementFactoryBase
+    public string Path
+    {
+        get
         {
-            public override UIObject CreateUIObject(UIObject parent, object value, object context)
+            fixed (byte* pathPtr = path)
             {
-                FileInfo fileInfo = (FileInfo)value;
-
-                UIButton button = parent.Spawn<UIButton>(new Name("Button"));
-                button.SetSize(new UIObjectSize(100, UIObjectSizeFlags.Percent, 100, UIObjectSizeFlags.Percent));
-                button.SetText(fileInfo.Name);
-                button.SetBorderRadius(0);
-                button.SetBackgroundColor(new Color(0));
-                
-                return button;
-            }
-
-            public override void UpdateUIObject(UIObject uiObject, object value, object context)
-            {
-                FileInfo fileInfo = (FileInfo)value;
-
-                UIButton button = (UIButton)uiObject;
-                button.SetText(fileInfo.Name);
+                return new string((sbyte*)pathPtr);
             }
         }
-
-        public class FileBrowserDialog : UIEventHandler
+        set
         {
-            private string currentPath = "";
-            private UIGrid? contentsGrid;
-            private UIPanel? noDataPanel;
-            private UIButton? backButton;
-            private Stack<string> pathStack = new Stack<string>();
-            private string filter = "*.obj";
-
-            public override void Init(Entity entity)
+            fixed (byte* pathPtr = path)
             {
-                base.Init(entity);
-
-                Logger.Log(LogType.Info, "FileBrowserDialog initialized");
-            }
-
-            [UIEvent(AllowNested = true)]
-            public override UIEventHandlerResult OnAttached()
-            {
-                Logger.Log(LogType.Info, "OnAttached");
-
-                HypClass hypClass = HypClass.GetClass<FileInfo>();
-
-                contentsGrid = UIObject.Find<UIGrid>(new Name("File_Browser_Dialog_Contents", weak: true));
-                contentsGrid?.SetIsVisible(false);
-
-                UIDataSource dataSource = new UIDataSource(TypeId.ForType<FileInfo>(), new FileInfoUIElementFactory());
-                contentsGrid?.SetDataSource(dataSource);
-
-                noDataPanel = UIObject.Find<UIPanel>(new Name("File_Browser_Dialog_NoData_Panel", weak: true));
-                noDataPanel?.SetIsVisible(true);
-
-                backButton = UIObject.Find<UIButton>(new Name("File_Browser_Dialog_Back_Button", weak: true));
-                backButton?.SetIsVisible(false);
-
-                PushDirectory(Directory.GetCurrentDirectory());
-
-                return UIEventHandlerResult.Ok;
-            }
-
-            [UIEvent]
-            public UIEventHandlerResult BackButtonClicked()
-            {
-                Logger.Log(LogType.Info, "Back button clicked");
-
-                PopDirectory();
-
-                return UIEventHandlerResult.Ok;
-            }
-
-            private void PushDirectory(string path)
-            {
-                pathStack.Push(currentPath);
-                currentPath = path;
-                UpdateContents();
-
-                backButton?.SetIsVisible(pathStack.Count > 1);
-            }
-
-            private void PopDirectory()
-            {
-                if (pathStack.Count > 0)
+                for (int i = 0; i < 4096; i++)
                 {
-                    currentPath = pathStack.Pop();
-                    UpdateContents();
+                    path[i] = 0;
                 }
 
-                backButton?.SetIsVisible(pathStack.Count > 1);
-            }
-
-            private void UpdateContents()
-            {
-                List<FileInfo> files = GetFiles(currentPath);
-
-                UIDataSourceBase? dataSource = contentsGrid?.GetDataSource();
-                Assert.Throw(dataSource != null, "DataSource is null");
-
-                dataSource.Clear();
-
-                foreach (FileInfo fileInfo in files)
+                for (int i = 0; i < value.Length; i++)
                 {
-                    dataSource.Push(new UUID(), fileInfo);
-                }
-
-                if (dataSource.Size() == 0)
-                {
-                    contentsGrid?.SetIsVisible(false);
-                    noDataPanel?.SetIsVisible(true);
-                }
-                else
-                {
-                    contentsGrid?.SetIsVisible(true);
-                    noDataPanel?.SetIsVisible(false);
-                }
-            }
-
-            private List<FileInfo> GetFiles(string path)
-            {
-                List<FileInfo> files = new List<FileInfo>();
-
-                try
-                {
-                    foreach (string directory in Directory.GetDirectories(path))
+                    if (i == 4095)
                     {
-                        files.Add(new FileInfo
-                        {
-                            Name = Path.GetFileName(directory),
-                            Path = directory,
-                            IsDirectory = true
-                        });
+                        break;
                     }
 
-                    // Files
-                    string[] filters = filter.Split(';', StringSplitOptions.RemoveEmptyEntries);
-                    foreach (string file in Directory.GetFiles(path))
-                    {
-                        string fileExtension = Path.GetExtension(file).ToLowerInvariant();
-                        bool matchesFilter = false;
-
-                        foreach (string f in filters)
-                        {
-                            string filterExtension = Path.GetExtension(f).ToLowerInvariant();
-                            if (filterExtension.Equals(fileExtension, StringComparison.OrdinalIgnoreCase))
-                            {
-                                matchesFilter = true;
-                                break;
-                            }
-                        }
-
-                        if (!matchesFilter)
-                        {
-                            continue;
-                        }
-
-                        files.Add(new FileInfo
-                        {
-                            Name = Path.GetFileName(file),
-                            Path = file,
-                            IsDirectory = false
-                        });
-                    }
+                    path[i] = (byte)value[i];
                 }
-                catch (Exception e)
-                {
-                    Logger.Log(LogType.Error, $"Error getting files: {e.Message}");
-                }
-
-                return files;
             }
         }
+    }
+
+    public bool IsDirectory
+    {
+        get
+        {
+            return isDirectory;
+        }
+        set
+        {
+            isDirectory = value;
+        }
+    }
+}
+
+public class FileInfoUIElementFactory : UIElementFactoryBase
+{
+    public override UIObject CreateUIObject(UIObject parent, object value, object context)
+    {
+        FileInfo fileInfo = (FileInfo)value;
+
+        UIButton button = parent.Spawn<UIButton>(new Name("Button"));
+        button.SetSize(new UIObjectSize(100, UIObjectSizeFlags.Percent, 100, UIObjectSizeFlags.Percent));
+        button.SetText(fileInfo.Name);
+        button.SetBorderRadius(0);
+        button.SetBackgroundColor(new Color(0));
+        
+        return button;
+    }
+
+    public override void UpdateUIObject(UIObject uiObject, object value, object context)
+    {
+        FileInfo fileInfo = (FileInfo)value;
+
+        UIButton button = (UIButton)uiObject;
+        button.SetText(fileInfo.Name);
+    }
+}
+
+public class FileBrowserDialog : UIEventHandler
+{
+    private string currentPath = "";
+    private UIGrid? contentsGrid;
+    private UIPanel? noDataPanel;
+    private UIButton? backButton;
+    private Stack<string> pathStack = new Stack<string>();
+    private string filter = "*.obj";
+
+    public override void Init(Entity entity)
+    {
+        base.Init(entity);
+
+        Logger.Log(LogType.Info, "FileBrowserDialog initialized");
+    }
+
+    [UIEvent(AllowNested = true)]
+    public override UIEventHandlerResult OnAttached()
+    {
+        Logger.Log(LogType.Info, "OnAttached");
+
+        HypClass hypClass = HypClass.GetClass<FileInfo>();
+
+        contentsGrid = UIObject.Find<UIGrid>(new Name("File_Browser_Dialog_Contents", weak: true));
+        contentsGrid?.SetIsVisible(false);
+
+        UIDataSource dataSource = new UIDataSource(TypeId.ForType<FileInfo>(), new FileInfoUIElementFactory());
+        contentsGrid?.SetDataSource(dataSource);
+
+        noDataPanel = UIObject.Find<UIPanel>(new Name("File_Browser_Dialog_NoData_Panel", weak: true));
+        noDataPanel?.SetIsVisible(true);
+
+        backButton = UIObject.Find<UIButton>(new Name("File_Browser_Dialog_Back_Button", weak: true));
+        backButton?.SetIsVisible(false);
+
+        PushDirectory(Directory.GetCurrentDirectory());
+
+        return UIEventHandlerResult.Ok;
+    }
+
+    [UIEvent]
+    public UIEventHandlerResult BackButtonClicked()
+    {
+        Logger.Log(LogType.Info, "Back button clicked");
+
+        PopDirectory();
+
+        return UIEventHandlerResult.Ok;
+    }
+
+    private void PushDirectory(string path)
+    {
+        pathStack.Push(currentPath);
+        currentPath = path;
+        UpdateContents();
+
+        backButton?.SetIsVisible(pathStack.Count > 1);
+    }
+
+    private void PopDirectory()
+    {
+        if (pathStack.Count > 0)
+        {
+            currentPath = pathStack.Pop();
+            UpdateContents();
+        }
+
+        backButton?.SetIsVisible(pathStack.Count > 1);
+    }
+
+    private void UpdateContents()
+    {
+        List<FileInfo> files = GetFiles(currentPath);
+
+        UIDataSourceBase? dataSource = contentsGrid?.GetDataSource();
+        Assert.Throw(dataSource != null, "DataSource is null");
+
+        dataSource.Clear();
+
+        foreach (FileInfo fileInfo in files)
+        {
+            dataSource.Push(new UUID(), fileInfo);
+        }
+
+        if (dataSource.Size() == 0)
+        {
+            contentsGrid?.SetIsVisible(false);
+            noDataPanel?.SetIsVisible(true);
+        }
+        else
+        {
+            contentsGrid?.SetIsVisible(true);
+            noDataPanel?.SetIsVisible(false);
+        }
+    }
+
+    private List<FileInfo> GetFiles(string path)
+    {
+        List<FileInfo> files = new List<FileInfo>();
+
+        try
+        {
+            foreach (string directory in Directory.GetDirectories(path))
+            {
+                files.Add(new FileInfo
+                {
+                    Name = Path.GetFileName(directory),
+                    Path = directory,
+                    IsDirectory = true
+                });
+            }
+
+            // Files
+            string[] filters = filter.Split(';', StringSplitOptions.RemoveEmptyEntries);
+            foreach (string file in Directory.GetFiles(path))
+            {
+                string fileExtension = Path.GetExtension(file).ToLowerInvariant();
+                bool matchesFilter = false;
+
+                foreach (string f in filters)
+                {
+                    string filterExtension = Path.GetExtension(f).ToLowerInvariant();
+                    if (filterExtension.Equals(fileExtension, StringComparison.OrdinalIgnoreCase))
+                    {
+                        matchesFilter = true;
+                        break;
+                    }
+                }
+
+                if (!matchesFilter)
+                {
+                    continue;
+                }
+
+                files.Add(new FileInfo
+                {
+                    Name = Path.GetFileName(file),
+                    Path = file,
+                    IsDirectory = false
+                });
+            }
+        }
+        catch (Exception e)
+        {
+            Logger.Log(LogType.Error, $"Error getting files: {e.Message}");
+        }
+
+        return files;
     }
 }
